d25e3ca86d152fc6345511cc5e46dccf9cd3e620
manhattien <tienmanhat11a321@gmail.com>
2025-11-11 06:44:40 +0700

    Merge main into feature/tien and resolve conflicts

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -6,7 +6,7 @@ import ProfilePage from "./Page/ProfilePage"
 import AuthProvider from "./ContextAPI/AuthProvider"
 // 1. Import ProtectedRoute
 import ProtectedRoute from "./ContextAPI/ProtectedRoute"
-import RegisterCoursePage from "./Page/RegisterCoursePage"
+import RegisterCoursePage from "./Page/Student_CourseRegistration"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
